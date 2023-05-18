8277b3258dcb8043c1171f7fca54820f06c80971
MavileSeytoplaeva <mavile.seitoplaeva@gmail.com>
2023-05-18 19:34:45 +0300

    Initial commit

---
+++ src/main/java/pro/sky/courseworkweb1/CourseWorkWeb1Application.java
@@ -0,0 +1,13 @@
+package pro.sky.courseworkweb1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWorkWeb1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseWorkWeb1Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/courseworkweb1/CourseWorkWeb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.courseworkweb1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWorkWeb1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
